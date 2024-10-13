0c80637fa42092030cebf046007ea4f0d00e6be4
Isaias Silva <108680376+Isaiaspereiraes@users.noreply.github.com>
2024-10-12 23:13:00 -0300

    100DoC.io D01, Hello World! and comment line's.

---
+++ java-100doc/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is a SINGLE line comment
+        System.out.println("Hello World !!!"); //This is ALSO a single line comment
+        /*
+        This is a MULTI line COMMENT
+         */
+    }
+}
